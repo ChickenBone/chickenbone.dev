69edcc7016ede21fc3937646567e62cddc28883a
Wyatt Whorton <bluebomber019@gmail.com>
2023-09-23 14:26:57 -0600

    add fish stocking report lol

---
+++ src/app/fishing/page.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+interface Stocking {
+    stocking_id: number,
+    body_of_water: string,
+    water_id: number,
+    region: string,
+    report_date: string
+}
+
+
+//Example fishing data
+// [{"stocking_id":1,"body_of_water":"Evergreen Lake","water_id":548,"region":"northeast","report_date":"2014-04-29"},{"stocking_id":2,"body_of_water":"Georgetown Lake","water_id":469,"region":"northeast","report_date":"2014-04-29"},{"stocking_id":3,"body_of_water":"Dowdy Lake","water_id":229,"region":"northeast","report_date":"2014-04-22"},{"stocking_id":4,"body_of_water":"West Lake","water_id":463,"region":"northeast","report_date":"2014-04-22"},{"stocking_id":5,"body_of_water":"Blue River (Section 2)","water_id":664,"region":"northeast","report_date":"2014-04-22"},]
+// Get body of water location from water_id
+// 
+
+
+export default function Fishing() {
+    const [stocking, setStocking] = useState<Stocking[]>([])
+
+    useEffect(() => {
+        axios.get("https://cpw.crestonedigital.com/fishing/api/v1/stocking").then((data) => {
+
+            // Cut out data from not the last month
+            let lastMonth = new Date()
+            lastMonth.setMonth(lastMonth.getMonth() - 1)
+            let lastMonthString = lastMonth.toISOString().split('T')[0]
+
+            let filteredData = data.data.filter((stock: Stocking) => {
+                return stock.report_date > lastMonthString
+            })
+
+            setStocking(filteredData)
+        })
+    }, [])
+
+
+    return (
+        <div className={'w-full h-full'}>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-16 place-items-center'>
+                <div className='col-span-1 md:col-span-2 place-self-start'>
+                    <h1 className='text-4xl font-bold text-center'>Colorado Stocking Report</h1>
+                </div>
+
+                {
+                    stocking.length == 0 && <div className='col-span-1 md:col-span-2 place-self-start'>
+                        <h1 className='text-4xl font-bold text-center'>Loading...</h1>
+                    </div>
+                }
+                {
+                    stocking.reverse().map((stock, index) => {
+                        return (
+                            <div className={`col-span-1 w-2/3 h-48 rounded-3xl shadow-lg p-4 bg-opacity-30
+                                ${stock.region == 'northeast' && 'bg-blue-200'}
+                                ${stock.region == 'northwest' && 'bg-green-200'}
+                                ${stock.region == 'southeast' && 'bg-yellow-200'}
+                                ${stock.region == 'southwest' && 'bg-red-200'}
+                                `}>
+                                <div className='flex flex-col gap-4 align-middle justify-center'>
+                                    <div className='col-span-1 md:col-span-2 '>
+                                        <h2 className='text-2xl font-bold text-center'>{stock.body_of_water}</h2>
+                                        <h3 className='text-xl font-bold text-center'>{stock.region}</h3>
+                                        <h4 className='text-lg font-bold text-center'>{stock.report_date}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+
+
+    )
+}

13a8243bf6bb4c0ff0c54f1bfb858cc1fcb34c4c
Wyatt Whorton <bluebomber019@gmail.com>
2023-10-03 12:56:25 -0600

    fix backdrop and safari compatability

--- src/components/backdrop/backdrop.tsx
+++ src/components/backdrop/backdrop.tsx
@@ -12,22 +12,28 @@ interface BackdropProps {
 export const Backdrop = (Props: BackdropProps) => {
     return (
         <>
-            <div className='z-20 bg-none'>
-                {Props.children}
-            </div>
-            <div className='overflow-hidden absolute -top-20 -left-20 -z-10 w-screen h-full blur-[200px] grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-24'>
+            <div className='overflow-hidden absolute -top-20 -left-20 -z-10 w-screen h-full blur-3xl min-w-full'
+            style={
                 {
-                    Array(12).fill(0).map((_, i) => {
+                    transform: "translate3d(0, 0, 0)" // Dirty hack to force the backdrop to render on GPU on Safari
+                }
+            }>
+                <div className='w-full h-full grid  grid-cols-1 md:grid-cols-3 justify-center items-center place-items-center'>
+                {
+                    Array(32).fill(0).map((_, i) => {
                         return (
                             <Circle
                                 key={i}
-                                color={i % 2 == 0 ? "bg-purple-500" : "bg-blue-500"}
+                                color={['bg-blue-500', 'bg-purple-500', 'bg-cyan-500'][Math.floor(Math.random() * 3)]}
                             />
                         )
                     })
                 }
+                </div>
             </div>
-
+            <div className='z-20 bg-none'>
+                {Props.children}
+            </div>
         </>
     )
 }
--- src/components/backdrop/circles/index.tsx
+++ src/components/backdrop/circles/index.tsx
@@ -6,8 +6,37 @@ interface CircleProps {
 }
 
 export const Circle = (Props: CircleProps) => {
+    // Randomly move the circles around the screen continuously picking an initial position, speed, and direction
+    // bounce off the edges of the screen ensuring the transition is smooth
+    const [x, setX] = React.useState(Math.random() * 100)
+    const [y, setY] = React.useState(Math.random() * 100)
+    const [speed, setSpeed] = React.useState(Math.random() * 0.5)
+    const [direction, setDirection] = React.useState(Math.random() * 360)
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            if (x > 100 || x < 0) {
+                setDirection(Math.random() * 360)
+            }
+            if (y > 100 || y < 0) {
+                setDirection(Math.random() * 360)
+            }
+            setX(x + speed * Math.cos(direction))
+            setY(y + speed * Math.sin(direction))
+        }, 10)
+        return () => clearInterval(interval)
+    }, [x, y, speed, direction])
+
+
+
+
     return (
-        <div className={`min-w-96 min-h-96 rounded-full ${Props.color}`}>
+        <div className={`w-[1100px] h-[900px] rounded-full ${Props.color}`} 
+            style={{
+                transform: `translate(${x}vw, ${y}vh)`,
+                transition: 'all 0.1s ease-in-out'
+            }}
+        >
         </div>
     )
 }
